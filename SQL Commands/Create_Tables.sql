533262b300304ffd4600720b08db740bb140fbf0
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-06-06 23:00:27 -0400

    Add SQL scripts for dimension and performance tables

--- SQL Commands/Create_Tables.sql
+++ SQL Commands/Create_Tables.sql
@@ -1,6 +1,58 @@
--- Create Replays Table
+-- Create dim_Maps (DIMENSION)
 
-IF OBJECT_ID('dbo.Replays', 'U') IS NULL
+IF OBJECT_ID('dbo.dim_Maps', 'U') IS NULL
+BEGIN
+    CREATE TABLE dim_Maps(
+        
+    );
+END
+GO
+
+-- Create dim_Playlists (DIMENSION)
+
+IF OBJECT_ID('dbo.dim_Playlists', 'U') IS NULL
+BEGIN
+    CREATE TABLE dim_Playlists(
+        
+    );
+END
+GO
+
+-- Create dim_Dates (DIMENSION)
+
+IF OBJECT_ID('dbo.dim_Dates', 'U') IS NULL
+BEGIN
+    CREATE TABLE dim_Dates(
+        
+    );
+END
+GO
+
+-- Create dim_Seasons (DIMENSION)
+
+IF OBJECT_ID('dbo.dim_Seasons', 'U') IS NULL
+BEGIN
+    CREATE TABLE dim_Seasons(
+        
+    );
+END
+GO
+
+-- Create dim_Players Table (DIMENSION)
+
+IF OBJECT_ID('dbo.dim_Players', 'U') IS NULL
+BEGIN
+    CREATE TABLE Players(
+        player_id        VARCHAR(200) PRIMARY KEY,
+        player_name      VARCHAR(200),
+        platform         VARCHAR(30)
+    );
+END
+GO
+
+-- Create Replays Table (FACT)
+
+IF OBJECT_ID('dbo.fact_Replays', 'U') IS NULL
 BEGIN
     CREATE TABLE Replays(
         replay_id        VARCHAR(200) PRIMARY KEY,
@@ -22,22 +74,9 @@ BEGIN
 END
 GO
 
+-- Create Match Stats Table (FACT)
 
--- Create Players Table
-
-IF OBJECT_ID('dbo.Players', 'U') IS NULL
-BEGIN
-    CREATE TABLE Players(
-        player_id        VARCHAR(200) PRIMARY KEY,
-        player_name      VARCHAR(200),
-        platform         VARCHAR(30)
-    );
-END
-GO
-
--- Create Match Stats Table
-
-IF OBJECT_ID('dbo.Match_Stats', 'U') IS NULL
+IF OBJECT_ID('dbo.fact_Match_Stats', 'U') IS NULL
 BEGIN
     CREATE TABLE Match_Stats(
         replay_id        VARCHAR(200) NOT NULL,
@@ -50,6 +89,56 @@ BEGIN
 
         FOREIGN KEY (replay_id) REFERENCES Replays(replay_id),
         FOREIGN KEY (player_id) REFERENCES Players(player_id)
+    );
+END
+GO
+
+-- Create Players Performance Table (GOLD)
+
+IF OBJECT_ID('dbo.gold_Player_Performance', 'U') IS NULL
+BEGIN
+    CREATE TABLE gold_Player_Performance(
+        
+    );
+END
+GO
+
+-- Create Map Performance Table (GOLD)
+
+IF OBJECT_ID('dbo.gold_Map_Performance', 'U') IS NULL
+BEGIN
+    CREATE TABLE gold_Map_Performance(
+        
+    );
+END
+GO
+
+-- Create Playlist Performance Table (GOLD)
+
+IF OBJECT_ID('dbo.gold_Playlist_Performance', 'U') IS NULL
+BEGIN
+    CREATE TABLE gold_Playlist_Performance(
+        
+    );
+END
+GO
+
+-- Create Season Performance Table (GOLD)
+
+IF OBJECT_ID('dbo.gold_Season_Performance', 'U') IS NULL
+BEGIN
+    CREATE TABLE gold_Season_Performance(
+        
+    );
+END
+GO
+
+-- Create Daily Metrics Table (GOLD)
+
+IF OBJECT_ID('dbo.gold_Daily_Metrics', 'U') IS NULL
+BEGIN
+    CREATE TABLE gold_Daily_Metrics(
+        
     );
 END
 GO
